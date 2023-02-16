3278a312a2973d405a9ae00b6350df3e140289af
AnaduCG <godzionwin@gmail.com>
2023-02-16 16:21:26 +0100

    Uplading

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-/*The main function prints out some strings and return 0 errors on successfule execution*/
-int main(void){
+/*The function prints out given strings and return 0 errors on successfule execution*/
+int main(void)
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+	return (0);
 }
