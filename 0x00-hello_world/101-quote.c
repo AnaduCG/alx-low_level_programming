b2453da30de8c192509d79e6f005d0ece8246bcf
AnaduCG <godzionwin@gmail.com>
2023-02-16 18:02:12 +0100

    Updated

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ *main - entry path for c programs
+ *Return: returns 0 if successful
+ */
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	
+	return(1);
+}
