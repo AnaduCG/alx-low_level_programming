ece49cfe144e9550468f4bff7cb7db10fd7b3de7
AnaduCG <godzionwin@gmail.com>
2023-02-16 17:45:15 +0100

    Updated

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,11 +11,11 @@ int main(void)
 	long long int var_llint;
 	float var_float;
 
-	printf("Size of a char: %s byte(s)\n", sizeof(var_char));
-	printf("Size of a int: %d byte(s)\n", sizeof(var_int));
+	printf("Size of a char: %ld byte(s)\n", sizeof(var_char));
+	printf("Size of a int: %ld byte(s)\n", sizeof(var_int));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(var_lint));
-	printf("Size of a long long int: %lld byte(s)\n", sizeof(var_llint));
-	printf("Size of a float: %f byte(s)\n", sizeof(var_float));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(var_llint));
+	printf("Size of a float: %ld byte(s)\n", sizeof(var_float));
 
 	return (0);
 }
