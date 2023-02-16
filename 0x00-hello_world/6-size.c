152fcb25ebe787a39e75f9fe01eb0de55d72c782
AnaduCG <godzionwin@gmail.com>
2023-02-16 17:33:36 +0100

    Updated

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/*
+ *main - is the entry path in C programming
+ *return: returns 0 errors if there is none
+ */
+int main(void)
+{
+	char = var_car;
+	int = var_int;
+	long int = var_lint;
+	long long int =	var_llint;
+	float = var_float;
+	
+	printf("Size of a char: %s byte(s)\n", sizeof(var_char));
+	printf("Size of a int: %d byte(s)\n", sizeof(var_int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(var_lint));
+	printf("Size of a long long int: %lld byte(s)\n", sizeof(var_llint));
+	printf("Size of a float: %f byte(s)\n", sizeof(var_float));
+
+	retun (0);
+}
