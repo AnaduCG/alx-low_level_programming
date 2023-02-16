113f7c2fc517d2d64c5f8af56a8611f0cc0dcf34
AnaduCG <godzionwin@gmail.com>
2023-02-16 16:40:03 +0100

    Uplading

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
 *main - Is an entry point for c programs
-*return - returns 0 if the program is successful
+*Return: returns 0 if the program is successful
 */
 int main(void)
 {
-	printf("th proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
