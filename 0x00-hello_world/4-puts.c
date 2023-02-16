15e775ffdb7cb3111f92456d209a5196e7fc5381
AnaduCG <godzionwin@gmail.com>
2023-02-16 16:51:39 +0100

    Uplading

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+/**
+ *main - is an entry point for c progrms
+ * */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
