f5546d6d5ee07c864f0a6e596b772f44a40e7cf2
AnaduCG <godzionwin@example.com>
2023-02-17 07:31:07 -0800

    Uplading

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Return: always 0 (successful)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i)
+	}
+
+	return (0);
+}
