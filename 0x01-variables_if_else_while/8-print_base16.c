7835e2643908de54d43f9223dc83b323d6f7819d
AnaduCG <godzionwin@example.com>
2023-02-17 07:59:02 -0800

    Uplaoding

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * Return: alwasys 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 16; i++)
+	{
+
+		if (i < 10)
+		{
+			putchar(i + '0');
+		}
+		else
+		{
+			putchar(i - 10 + ''a);
+		}
+	}
+
+	return (0);
+}
