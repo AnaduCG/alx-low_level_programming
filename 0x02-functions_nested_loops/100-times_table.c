0e9286319abd8588eb16fcca7d0053d6b3ecb79e
AnaduCG <godzionwin@gmail.com>
2023-02-22 00:22:05 +0100

    Uploading

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,19 +12,35 @@ void print_times_table(int n)
 	if (n > 15 || n < 0)
 	{
 	}
-	else{
-		for (count = 0; count <= n; count ++)
+	else
+	{
+		for (count = 0; count <= n; count++)
 		{
-			for (times = 0; times <= n; times ++)
+			for (times = 0; times <= n; times++)
 			{
+				int table = (times * count);
+
 				if (times != n)
 				{
-					printf("%d,   ", times * count);
+					if (table >= 0 && table < 10)
+					{
+						printf("%d,...", table);
+					}
+					else if (table > 9 && table < 100)
+					{
+						printf("%d,..", table);
+
+					}
+					else if (table > 99 && table < 1000)
+					{
+						printf("%d,.", table);
+
+					}
 				}
 				else
 				{
 					printf("%d", times * count);
-				}
+
 			}
 			printf("\n");
 		}
