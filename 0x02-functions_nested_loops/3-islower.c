a471c417802646027c9e4d16ae4005919e863ec3
AnaduCG <godzionwin@gmail.com>
2023-02-21 14:47:59 +0100

    Uploading

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_islower - checks the character case of c
+ *Return: 1 if upper and 0 otherwise
+ */
+
+int _islower(int c)
+{
+	if (islower(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
