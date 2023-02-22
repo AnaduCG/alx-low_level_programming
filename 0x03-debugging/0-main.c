88e1e98dbf41469c72c09693b10e11e73e342d43
AnaduCG <godzionwin@gmail.com>
2023-02-22 22:47:26 +0100

    Pushing to github

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- *main - entry point
+ *main - entry point in c programing
  *Return: always 0
- */
+*/
 
 int main(void)
 {
-	int i = 0;
+	int i;
 
+	i = 0;
 	positive_or_negative(i);
 
 	return (0);
