afa1ba076f34917d166e462434f5eaee5386826c
AnaduCG <godzionwin@gmail.com>
2023-02-22 21:18:22 +0100

    Finally

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,31 +7,18 @@
 
 void jack_bauer(void)
 {
-	int h1, h2, m1, m2;
+	int h, m;
 
-	for (h1 = 0; h1 < 3; h1++)
+	for (h = 0; h < 24; h++)
 	{
-		for (h2 = 0; h2 < 4; h2++)
+		for (m = 0; m < 60; m++)
 		{
-			for (m1 = 0; m1 < 6; m1++)
-			{
-				for (m2 = 0; m2 < 10; m2++)
-				{
-					putchar(h1 + '0');
-					putchar(h2 + '0');
-					putchar(':');
-					putchar(m1 + '0');
-					putchar(m2 + '0');
-					if (h1 == 2 && h2 == 3 && m1 == 5 && m2 == 9)
-					{
-						continue;
-					}
-					else
-					{
-						putchar('\n');
-					}
-				}
-			}
+			_putchar((h / 10) + 48);
+			_putchar((h % 10) + 48);
+			_putchar(':');
+			_putchar((m / 10) + 48);
+			_putchar((m % 10) + 48);
+			_putchar('\n');
 		}
 	}
 }
