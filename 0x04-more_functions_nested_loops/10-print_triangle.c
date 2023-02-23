fd33258cd8255b505b28e11a42a2c230f8749615
AnaduCG <godzionwin@gmail.com>
2023-02-23 21:43:44 +0100

    still Learning

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,5 +6,20 @@
 
 void print_triangle(int size)
 {
-	int 
+	int count, space, item;
+
+	if (size <= 0)
+		putchar('\n');
+	for (count = 1; count <= size; count++)
+	{
+		for (space = count; space < size; space++)
+		{
+			putchar(' ');
+		}
+		for (item = 1; item <= count; item++)
+		{
+			putchar('#');
+		}
+		putchar('\n');
+	}
 }
