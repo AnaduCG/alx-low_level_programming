ecb15266d5ed55b3dcde5b6c5620ddb1cbb232b1
AnaduCG <godzionwin@gmail.com>
2023-03-02 19:58:52 +0100

    Am loving this :)

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,9 +1,22 @@
 #include "main.h"
 
 /**
- *
- *
- *
+ *leet - substitutes characters
+ *@str: a pointer parameter of typechar
+ *Return: returns substituted characters string
  */
-char *leet(char *)
-{}
+char *leet(char *str)
+{
+	char nums[5] = {'4', '3', '0', '7', '1'};
+	char small_str[5] = {'a', 'e', 'o', 't', 'l'};
+	char big_str[5] = {'A', 'E', 'O', 'T', 'L'};
+	int i, n, len = (strlen(str));
+
+	for (i = 0; i < len; i++)
+	{
+		for (n = 0; n < 5; n++)
+			if (str[i] == small_str[n] || str[i] == big_str[n])
+				str[i] = nums[n];
+	}
+	return (str);
+}
