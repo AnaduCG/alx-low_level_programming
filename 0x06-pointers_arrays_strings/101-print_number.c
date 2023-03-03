a914875d5dc6f5036a327f669c3b2f31b21ec8c2
AnaduCG <godzionwin@gmail.com>
2023-03-03 16:25:56 +0100

    optimized code

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -5,15 +5,16 @@
  */
 void print_number(int n)
 {
+	unsigned int i = n;
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		i = -i;
 	}
 	if (n / 10)
 	{
-		print_number(n / 10);
+		print_number(i / 10);
 	}
-		n = ((n % 10) + '0');
+		n = ((i % 10) + '0');
 		_putchar(n);
 }
