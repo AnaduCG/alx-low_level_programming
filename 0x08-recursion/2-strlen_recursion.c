4fffe313e87bc42e5c1dc6f93534012fcbdc7ffa
AnaduCG <godzionwin@gmail.com>
2023-03-07 12:48:28 +0100

    Uplaoding to github

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *_strlen_recursion - gets the length of a tring
+ *@s: variable parameter pointer of type char
+ *Return: returns the string length
+ */
+int _strlen_recursion(char *s)
+{
+	return (strlen(s));
+}
