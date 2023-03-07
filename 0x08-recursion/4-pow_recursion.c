3aaf16360e4bbf5e2f7968a34dddb1c61712941e
AnaduCG <godzionwin@gmail.com>
2023-03-07 19:02:14 +0100

    Uplaoding to github

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <math.h>
+/**
+ *int _pow_recursion - raises x to the power of y
+ *@x: parameter variable of type int
+ *@y: parameter variable of type int
+ *Return: returns x to the power of y
+ */
+int _pow_recursion(int x, int y)
+{
+	unsigned int i = pow(x, y);
+	return (i);
+}
