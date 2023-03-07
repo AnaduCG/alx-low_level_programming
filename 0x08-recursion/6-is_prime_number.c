56702e19f879132f23ded8334fc5c463edbee4f0
AnaduCG <godzionwin@gmail.com>
2023-03-07 23:08:51 +0100

    First upload

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
  * prime - checks for a divisible prime int
- * @n: variable type int
+ * @num1: variable type int
+ * @num2: variable type int
  * Return: 0 if not prime, 1 if prime
  */
 
@@ -26,14 +27,15 @@ int prime(int num1, int num2)
 
 int is_prime_number(int n)
 {
-    int num2 = 2;
-    if  (n <= 1)
-    {
-	    return (0);
-    }
-    if (n >= 2 && n <= 3)
-    {
-	    return (1);
-    }
-    return (prime(n, num2));
+	int num2 = 2;
+
+	if  (n <= 1)
+	{
+		return (0);
+	}
+	if (n >= 2 && n <= 3)
+	{
+		return (1);
+	}
+	return (prime(n, num2));
 }
