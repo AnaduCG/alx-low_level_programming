a6b219eb6acb63aa9afacab17842de8b123010e3
AnaduCG <godzionwin@gmail.com>
2023-03-07 22:43:27 +0100

    First upload

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- *
- *
- *
- *
+ *root - checks for the square root of an int
+ *@n: integer parameter
+ *@m: integer parameter
+ *Return: returns the square root of n and -1 otherwise
  */
-int root (int n, int m)
+int root(int n, int m)
 {
 	if ((m * m) == n)
 	{
@@ -14,18 +14,24 @@ int root (int n, int m)
 	}
 	else if ((m * m) > n)
 	{
-		return(-1);
+		return (-1);
 	}
-	return root(n, m + 1);
+	return (root(n, (m + 1)));
 }
+
+/**
+ *_sqrt_recursion - checks the value of n
+ *@n: integer parameter
+ *Return: returns the square root of n and -1 otherwise
+ */
 
 int _sqrt_recursion(int n)
 {
-	if ( n < 0)
+	if (n < 0)
 	{
 		return (-1);
 	}
-	else if (n == 0 || n == 1) 
+	else if (n == 0 || n == 1)
 	{
 		return (n);
 	}
