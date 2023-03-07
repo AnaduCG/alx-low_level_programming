f3ea2084772c039f1f4afd0dcffc0184e0bcc022
AnaduCG <godzionwin@gmail.com>
2023-03-07 23:02:39 +0100

    First upload

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,15 +7,15 @@
 
 int prime(int num1, int num2)
 {
-    if (num1 % num2 == 0)
-    {
-        return (0);
-    }
-    if (num2 == num1 / 2)
-    {
-        return (1);
-    }
-    return (prime(num1, num2 + 1));
+	if (num1 % num2 == 0)
+	{
+		return (0);
+	}
+	if (num2 == num1 / 2)
+	{
+		return (1);
+	}
+	return (prime(num1, num2 + 1));
 }
 
 /**
@@ -23,17 +23,17 @@ int prime(int num1, int num2)
  * @n: variable type int
  * Return: 0 if not prime, 1 if prime
  */
+
 int is_prime_number(int n)
 {
     int num2 = 2;
-
     if  (n <= 1)
-        {
-            return (0);
-        }
-        if (n >= 2 && n <= 3)
-        {
-            return (1);
-        }
-        return (prime(n, num2));
+    {
+	    return (0);
+    }
+    if (n >= 2 && n <= 3)
+    {
+	    return (1);
+    }
+    return (prime(n, num2));
 }
