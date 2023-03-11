059ccb5ee657aafdd50d69ebb9e4bdcd82accb48
AnaduCG <godzionwin@gmail.com>
2023-03-11 03:05:41 +0100

    Not yet done

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ *main - entry point in c programming
+ * @argc: count of arguments passed in the terminal
+ * @argv: the arguments passed in the terminal
+ * Return: always 0
+ */
+
 int main(int argc, char *argv[])
 {
-    int cents, quarters, totalCoins, dimes, nickels = 0, twopences, pennies;
-    if (argc != 2)
-    {
-        printf("Error\n");
-        return 1;
-    }
+	int cents, quarters, totalCoins, dimes, nickels = 0, twopences, pennies;
 
-    cents = atoi(argv[1]);
-
-    if (cents < 0)
-    {
-        printf("0\n");
-        return 0;
-    }
-
-    printf("%d\n", totalCoins);
-
-    return 0;
+	if (argc != 2)
+	{
+		printf("Error\n");
+	return (1);
+	}
+	cents = atoi(argv[1]);
+	if (cents < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	printf("%d\n", totalCoins);
+	return (0);
 }
