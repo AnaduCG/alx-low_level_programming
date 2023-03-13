f6ca85178ab6ecab80739dea1a93529e9465711a
AnaduCG <godzionwin@gmail.com>
2023-03-13 22:09:46 +0100

    Formatting code properly

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,23 +1,25 @@
 #include "main.h"
 /**
  * free_grid - frees the memory of a 2 dimensional array
- * @grid: double pointer pointing to the 2 dimensional 
+ * @grid: double pointer pointing to the 2 dimensional
  *  array to be freed
- * 
+ *
  * @height: the height of the array
- * 
- * 
+ *
+ *
  */
 
 void free_grid(int **grid, int height)
 {
-    int i;
-    for (i = 0; i < height; i++)
-    {
-        if (grid[i] != NULL)
-        {
-            free(grid[i]);
-        }
-    }
-    free(grid);
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		if (grid[i] != NULL)
+		{
+			free(grid[i]);
+		}
+	}
+
+	free(grid);
 }
