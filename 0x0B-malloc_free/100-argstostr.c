02d5517314b42d644140b724ecfbff37f99dc7c8
AnaduCG <godzionwin@gmail.com>
2023-03-13 22:28:20 +0100

    Testing code

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int i, mem;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	mem = malloc(sizeof(char) * ac);
+	if (mem == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		mem[i] = av[i];
+		printf("%s", mem[i]);
+	}
+	return(mem);
+}
