310ec4d3285c38e8c09115156f2712460f96ca1b
AnaduCG <godzionwin@gmail.com>
2023-03-13 12:52:46 +0100

    Fixing timeout: segfualt dumped core

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,16 +10,26 @@ char *str_concat(char *s1, char *s2)
 	int len1, len2;
 	char *combined;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
 	len1 = strlen(s1);
 	len2 = strlen(s2);
+
 	combined = malloc(len1 + len2 + 1);
-	if (combined == NULL || s1 == NULL || s2 == NULL)
+
+	if (combined == NULL)
 	{
 		return (NULL);
 	}
 
 	strcpy(combined, s1);
 	strcat(combined, s2);
-
 	return (combined);
 }
