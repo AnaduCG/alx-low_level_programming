517e61dbade199f3227cb05002d6ac5b5a6feace
AnaduCG <godzionwin@gmail.com>
2023-03-13 12:37:23 +0100

    Fixing segfualt

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,13 +10,13 @@ char *str_concat(char *s1, char *s2)
 	int len1, len2;
 	char *combined;
 
-	if (s1 == NULL || s2 == NULL)
-	{
-		return (NULL);
-	}
 	len1 = strlen(s1);
 	len2 = strlen(s2);
 	combined = malloc(len1 + len2 + 1);
+	if (combined == NULL)
+	{
+		return (NULL);
+	}
 
 	strcpy(combined, s1);
 	strcat(combined, s2);
